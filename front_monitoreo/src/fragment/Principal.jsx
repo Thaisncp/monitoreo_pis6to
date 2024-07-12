eebe461a87cd8a71f6c12a025284288e8d9fff0d
lettcriss2003 <letty.rojas@unl.edu.ec>
2024-07-12 00:18:55 -0500

    Base Front

---
+++ front_monitoreo/src/fragment/Principal.jsx
@@ -0,0 +1,19 @@
+import BarraMenu from "./BarraMenu";
+
+const Principal = () => {
+
+
+    return (
+        <div>
+            <header>
+                <BarraMenu />
+            </header>
+            <section>
+                <h1>
+                    AQUI VA A IR LA PAGINA PRINCIPAL XDDD
+                </h1>
+            </section>
+        </div>
+    );
+}
+export default Principal;
